152dd62020d47ba31d5a88321c7c2756526949db
aqyuki <yksk171118@gmail.com>
2026-05-22 17:33:33 +0900

    chore(deps): :hammer: marp cliとprettierを追加

---
+++ marp.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@marp-team/marp-cli').Config} */
+const config = {
+  inputDir: './slides',
+  output: './dist',
+  themeSet: './themes',
+  pdf: true,
+}
+
+export default config
